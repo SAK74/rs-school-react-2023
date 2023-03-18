485b656e300c4388ee0d6ef139cae4ad627791cb
SAK74 <sergijks@gmail.com>
2023-03-18 18:32:42 +0100

    feat: add search component behavior & styling

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,6 +1,13 @@
-export const SearchBar = () => (
+import { ChangeEvent, FC } from "react";
+
+interface Props {
+  value: string;
+  onChange: (ev: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const SearchBar: FC<Props> = (props) => (
   <div className="search__bar">
-    <input type="text" placeholder="" />
+    <input type="text" placeholder="" {...props} />
     <button>Search</button>
   </div>
 );
