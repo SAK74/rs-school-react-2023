104f2f66439e8f84e8128a2224791c8d33c783ce
SAK74 <sergijks@gmail.com>
2023-03-19 20:14:57 +0100

    fix: test with coverage comand

--- src/__tests__/CardsList.test.tsx
+++ src/__tests__/CardsList.test.tsx
@@ -8,13 +8,13 @@ describe("Cards list testing", () => {
   afterEach(() => {
     cleanup();
   });
+
   it("Cards list should be rendered", () => {
-    // render(<CardsList />);
     const _list = screen.getByTestId("cards-list");
     expect(_list).toBeInTheDocument();
   });
+
   it("Cards list should render predefined number of cards", async () => {
-    // render(<CardsList />);
     const cards = await screen.findAllByTestId("wrapper");
     expect(cards).toHaveLength(10);
   });
