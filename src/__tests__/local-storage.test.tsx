89f46445499e4dd8f9f37a1288fdcabd528996bc
SAK74 <sergijks@gmail.com>
2023-03-23 22:31:34 +0100

    fix: eslint warning in tests

--- src/__tests__/local-storage.test.tsx
+++ src/__tests__/local-storage.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Search } from "components";
 
@@ -18,13 +18,12 @@ describe("local storage", () => {
   });
 
   it("should call setItem during unmount", async () => {
-    const { unmount, getByRole } = render(<Search />);
-    const input = getByRole("textbox");
+    const { unmount } = render(<Search />);
+    const input = screen.getByRole("textbox");
     const testString = "asdfgh";
-    await userEvent.type(input, testString);
+    userEvent.type(input, testString);
     unmount();
     expect(window.sessionStorage.setItem).toBeCalledTimes(1);
     expect(window.sessionStorage.setItem).toBeCalledWith("input", testString);
   });
-  it("", () => {});
 });
