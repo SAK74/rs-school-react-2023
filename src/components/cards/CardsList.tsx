b4bc52f8d8e517cef7c25cd5d1653155a8be8935
SAK74 <sergijks@gmail.com>
2023-04-08 20:43:37 +0200

    feat: add modal window

--- src/components/cards/CardsList.tsx
+++ src/components/cards/CardsList.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
+import { FC, ReactNode, useState } from "react";
 import { CardType, UserType, RickandmortyType } from "types";
 import { Card } from "./Card";
+import { Modal } from "./Modal";
 import { MortyCard } from "./MortCard";
 import "./style.scss";
 
@@ -10,21 +11,46 @@ interface ListProps {
 }
 
 export const CardsList: FC<ListProps> = (props) => {
+  const [show, setShow] = useState<false | CardType>(false);
   return (
-    <div className="card__container" data-testid="cards-list">
-      {props.cards?.map((elem, ind) =>
-        props.type === "user" ? (
-          <Card
-            key={ind + (elem as UserType).name.first}
-            user={elem as UserType}
-          />
-        ) : (
-          <MortyCard
-            key={(elem as RickandmortyType).id}
-            person={elem as RickandmortyType}
-          />
-        )
-      )}
-    </div>
+    <>
+      <Modal show={Boolean(show)} onClose={() => setShow(false)}>
+        {show && showEntries(show)}
+      </Modal>
+      <div className="card__container" data-testid="cards-list">
+        {props.cards?.map((elem, ind) =>
+          props.type === "user" ? (
+            <Card
+              key={ind + (elem as UserType).name.first}
+              user={elem as UserType}
+              onClick={() => setShow(elem)}
+            />
+          ) : (
+            <MortyCard
+              key={(elem as RickandmortyType).id}
+              person={elem as RickandmortyType}
+              onClick={() => {
+                console.log("click");
+                setShow(elem);
+              }}
+            />
+          )
+        )}
+      </div>
+    </>
   );
 };
+
+function showEntries(
+  elem: string | number | CardType | { [k: string]: string | number }
+): ReactNode {
+  if (typeof elem === "object") {
+    return Object.entries(elem).map(([name, value]) => (
+      <div key={name} className="line-of-list">
+        <span>{name}: </span>
+        {showEntries(value)}
+      </div>
+    ));
+  }
+  return elem;
+}
---
+++ src/components/cards/Modal.tsx
@@ -0,0 +1,28 @@
+import { FC, ReactNode } from "react";
+
+export const Modal: FC<{
+  show: boolean;
+  children?: ReactNode;
+  onClose: () => void;
+}> = ({ show, children, onClose }) => {
+  return (
+    <div
+      className="modal"
+      style={{ display: show ? "flex" : "none" }}
+      onClick={onClose}
+      data-testid="modal"
+    >
+      <div
+        className="modal__content"
+        onClick={(ev) => {
+          ev.stopPropagation();
+        }}
+      >
+        <span onClick={onClose} className="modal__close-button" title="close">
+          x
+        </span>
+        {children}
+      </div>
+    </div>
+  );
+};
