d46d611527ae1dc295a1327d78efe8afc7d17626
SAK74 <sergijks@gmail.com>
2023-04-09 23:43:14 +0200

    test: add SearchBar testing

---
+++ src/tests/SearchBar.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { SearchBar } from "components/search/SearchBar";
+
+describe("Search bar testing", () => {
+  beforeEach(() => {
+    render(<SearchBar value="" onChange={() => {}} onSearch={() => {}} />);
+  });
+  it("", () => {
+    const input = screen.getByRole("textbox");
+    const selects = screen.getAllByRole("combobox");
+    const btn = screen.getByRole("button", { name: "Search" });
+
+    expect(input).toBeInTheDocument();
+    expect(selects).toHaveLength(2);
+    expect(btn).toBeInTheDocument();
+  });
+});
